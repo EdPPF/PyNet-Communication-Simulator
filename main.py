e887fa54474880d01f42ff21ead795308d33606a
phbm2003 <145854692+phbm2003@users.noreply.github.com>
2025-01-26 18:42:50 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,35 @@ Invoca a função main() do Cliente ou do Servidor, dependendo da escolha do usu
 from server.server import start as start_server
 from client.client import start as start_client
 
+import tkinter as tk
+from servidor_gui import exibir_segunda_janela
+from cliente_gui import criar_janela_principal
+
 
 def main():
-    print("Selecione uma opção:")
-    print("1 - Iniciar Servidor")
-    print("2 - Iniciar Cliente")
-    option = input("Opção: ")
+    # print("Selecione uma opção:")
+    # print("1 - Iniciar Servidor")
+    # print("2 - Iniciar Cliente")
+    # option = input("Opção: ")
 
-    if option == "1":
-        start_server()
-    elif option == "2":
-        start_client()
-    else:
-        print("Opção inválida.")
+
+    root = tk.Tk()
+    root.title("Menu")
+    root.geometry("300x200")
+
+    tk.Label(root, text="Selecione uma opção:").pack(pady=10)
+
+    tk.Button(root, text="1 - Iniciar Servidor", command=start_server).pack(pady=10)
+    tk.Button(root, text="2 - Iniciar Cliente", command=start_client).pack(pady=10)
+
+    root.mainloop()
+
+    # if option == "1":
+    #     start_server()
+    # elif option == "2":
+    #     start_client()
+    # else:
+    #     print("Opção inválida.")
 
 if __name__ == "__main__":
     main()
